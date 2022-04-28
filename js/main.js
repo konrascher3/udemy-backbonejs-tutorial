a076b310ffdae4bae960f24ff3e9b3e54bf53103
Konrad Scherb <konrad.scherb@check24.de>
2022-04-28 11:29:22 +0200

    chore: add custom solution 2

--- js/main.js
+++ js/main.js
@@ -29,3 +29,39 @@ if(!car.isValid()) {
 } else {
     car.start()
 }
+
+// Models
+
+// Create Vehicles collection based on the vehicle method
+const Vehicles = Backbone.Collection.extend({
+    model: Vehicle
+})
+
+// Add cars inside the Vehicles collection
+
+//// Instantiate new Vehicle collection
+const vehicles = new Vehicles()
+
+//// Add new vehicle models to vehicle collection
+vehicles.add(new Vehicle({ registrationNumber: "XLI887", color: "Blue" }));
+vehicles.add(new Vehicle({ registrationNumber: "ZNP123", color: "Blue" }));
+vehicles.add(new Vehicle({ registrationNumber: "XUV456", color: "Gray" }));
+
+// Find all blue cars inside the collection
+const blueCars = vehicles.where({ color: "Blue" });
+console.log(blueCars)
+
+// Find the car with the registration number XLI887
+const specificCar = vehicles.findWhere({ registrationNumber: "XLI887" });
+console.log(specificCar.toJSON())
+
+// Remove the car with the registration number XLI887
+vehicles.remove(specificCar)
+
+// Convert the collection to a JSON object and log it in the console.
+console.log(vehicles.toJSON())
+
+// Iterate the collection and log each car in the console.
+for (let i = 0; i < vehicles.length; i++) {
+    console.log(vehicles.at(i).toJSON())
+}
